a5242f5f0977748a0cc8f2d2b996233d00dee526
unknown <martin.tran@hotmail.se>
2019-09-27 11:41:11 +0200

    updated with state functions

--- README.md
+++ README.md
@@ -2,12 +2,29 @@
 Learning react, testing and building single page application.
 
 ###### Basic react
-*React is all about creating component (basically custom html elements).
-*Every react component (a function) always has to return jsx code which can be rendered to the Dom.
-*React using lowercase reserved for html, so name variable with Uppercase in the first letter
+* React is all about creating components (basically custom html elements).
+* Every react component (a function) always has to return jsx code which can be rendered to the Dom.
+* React using lowercase reserved for html, so name variable with Uppercase in the first letter
 
 ###### jsx
-*Looks like html but is not => it's javascript.
+* Looks like html but is not => it's javascript.
+
+###### Props
+* Props can be named whatever and it's the argument you send to a function
+* Props can send a whole function/method into a function
+
 
 ###### State 
-*State makes it possible to change your component at runtime.
+* State makes it possible to change your component at runtime.
+* Use state with care meaning use functional component as often as possible
+* To use state: class-based component => "class extends Component" 
+* class extends component has been the only way to manage state but since React 16.8 there has been a new way: functional component with React hooks
+
+###### React hooks
+* Collection of functions exposed to you by React which you can use in functional components
+* Needs to import useState 
+* With hooks you should have multiple state calls instead of one big slice like class extends Component 
+
+###### Stateless vs Stateful
+* Stateless/presentational/dumb component is a function/class that doesn't manage states: use stateless as much as you can
+* Stateful/container/smart component is a function/class that manages states: use stateful only when needed
--- src/App.js
+++ src/App.js
@@ -1,18 +1,65 @@
 import React, { Component } from 'react'; 
 import './App.css';
-import Person from './Person/Person.js';
+import Person from './Person/Person.js'; 
 
 //creating javascript class with the keyword "class"
 //and using the "extends" keyword to inherhit from the component object/class
 class App extends Component {
+
+  //att definera state funkar bara i class som har extends Component
+  //man kan definera vad som helst i en state, tex har vi skapat en 
+  //array med personer
+  state = {
+    persons: [
+      { name: 'Martin', age: 25},
+      { name: 'Sara', age: 24},
+      { name: 'Optiplan', age: 16},
+    ],
+    cantTouchThis: 'Wont be affected in switchNameHandler if we dont directly define to do it'
+  }
+
+  //define a normal function
+  //using .setState() to allow update props in the DOM
+  //in the .setState() decides what to be updated
+  switchNameHandler = (newName) => {
+    this.setState({    
+      persons: [
+        { name: newName, age: 25},
+        { name: 'Sara', age: 24},
+        { name: 'Optiplan', age: 16}
+      ]
+    })
+  }
+
+  nameChangedHandler = (event) => {
+    this.setState({    
+      persons: [
+        { name: event.target.value, age: 25},
+        { name: 'Sara', age: 24},
+        { name: 'Optiplan', age: 16}
+      ]
+    })
+  }
+
+  //this call upon the state/functions in the class
   render() {
     return (
         <div className="App">
             <h1>Test of a React App</h1>
             <p>Adding a paragraph</p> 
-            <Person name="Martin" age="25" > Using children to display Lists </Person>
-            <Person name="Sara" age="24" > My hobbies: Adding children props </Person>
-            <Person name="Optiplan" age="12"> is everyone here children? </Person>
+            <button onClick={this.switchNameHandler.bind(this, 'Mar')}>Switch Name</button>
+            <Person 
+              name={this.state.persons[0].name} 
+              age={this.state.persons[0].age} 
+              //passing a whole fucking function
+              click={() => this.switchNameHandler('Tin!!!')} 
+              changed={this.nameChangedHandler}/> 
+            <Person 
+              name={this.state.persons[1].name} 
+              age={this.state.persons[1].age} > passing children </Person>
+            <Person 
+              name={this.state.persons[2].name} 
+              age={this.state.persons[2].name}> </Person>
         </div>
     );
   }
